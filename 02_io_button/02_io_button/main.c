3d373cb4e368925825f0d0186a5e6657d57de744
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-03-19 21:02:06 +0200

    update comments

--- 02_io_button/02_io_button/main.c
+++ 02_io_button/02_io_button/main.c
@@ -1,26 +1,6 @@
-/*
- * testProject.c
- *
- * Created: 16/03/2025 13:16:33
- * Author : nguyen
- */ 
-
 #include <avr/io.h>
 #define F_CPU 8000000UL
 #include <util/delay.h>
-
-/*
-unsigned char checkDirection(unsigned char dir) {	
-	unsigned char new_dir = dir;
-	if (PIND == 0xFE) {
-		_delay_ms(50);
-		if (PIND == 0xFE) new_dir ^= 0x01;
-		while (PIND==0xFE);
-	}
-	return new_dir;
-}
-*/
-
 
 int main(void){
 	/*
@@ -36,9 +16,7 @@ int main(void){
 	}
 	*/
 	/*
-	// Exercise 1: Make a program which displays Port B LEDs as 0x0x0x0x if
-	// PD0 buttons is pressed, and x0x0x0x0 if PD7 button is pressed. If no
-	// button is pressed, all LEDs are off
+	// Task 1: Make a program which displays Port B LEDs as 0x0x0x0x if PD0 buttons is pressed, and x0x0x0x0 if PD7 button is pressed. If no button is pressed, all LEDs are off
 	DDRB = 0xFF;
 	DDRD = 0x00;
 	
@@ -58,8 +36,9 @@ int main(void){
 		}
 	}
 	*/
+	
 	/*
-	// Exercise 2: Make a program which all LEDs are off at the start. It turns on Port B LEDs if PD0 button is clicked, the LEDs will be turned off if PD0 button is clicked again. And repeat this procedure. (You may need a delay to prevent bouncing)
+	// Task 2: Make a program which all LEDs are off at the start. It turns on Port B LEDs if PD0 button is clicked, the LEDs will be turned off if PD0 button is clicked again. And repeat this procedure. 
 	DDRB = 0xFF;
 	DDRD = 0x00;
 	
@@ -87,7 +66,7 @@ int main(void){
 	*/
 
 	/*	
-	// Exercise 3: Make a program which flushes LEDs of PB from 0 to 7 repeatedly. The direction of flush will be changed if PD0 button is clicked.
+	// Task 3: Make a program which flushes LEDs of PB from 0 to 7 repeatedly. The direction of flush will be changed if PD0 button is clicked.
 	DDRB = 0xFF;
 	DDRD = 0x00;
 	
@@ -120,7 +99,7 @@ int main(void){
 	}
 	*/
 
-	// Make a program which blinks PB0 at 10Hz. If any 2 buttons are pushed down, the program blinks PB7 at 20Hz.
+	// Task4: Make a program which blinks PB0 at 10Hz. If any 2 buttons are pushed down, the program blinks PB7 at 20Hz.
 	DDRB = 0xFF;
 	DDRD = 0x00;
 	
