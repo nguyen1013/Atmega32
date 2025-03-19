5002419d6fadd20210f4d88887efa38bc0ab8562
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-03-19 21:01:21 +0200

    create README.MD

--- 01_io_led/io_led/main.c
+++ 01_io_led/io_led/main.c
@@ -1,10 +1,3 @@
-/*
- * testProject.c
- *
- * Created: 16/03/2025 13:16:33
- * Author : nguyen
- */ 
-
 #include <avr/io.h>
 #define F_CPU 8000000UL
 #include <util/delay.h>
@@ -13,8 +6,8 @@
 int main(void)
 {
 	/*
-	// Exercise 1: Make a program which outputs Binary numbers from 00000000 to 11111111 on port B, 
-	// each number is displayed 500ms. 
+	// Task 1: Make a program which outputs Binary numbers from 00000000 to 11111111 on port B, each number is displayed 500ms. 
+	
 	DDRB = 0xFF;
 	
     while (1) 
@@ -28,8 +21,7 @@ int main(void)
 	*/
 	
 	/*
-	// Exercise 2: Make a program which outputs BCD numbers from 0 to 99 on port B. 
-	//Advanced: when displaying numbers from 0 to 9, the higher 4 LEDs should be OFF
+	// Task 2: Make a program which outputs BCD numbers from 0 to 99 on port B. When displaying numbers from 0 to 9, the higher 4 LEDs should be OFF
 	DDRB = 0xFF;
 		
 	while (1)
@@ -46,8 +38,8 @@ int main(void)
 		}
 	}
 	*/
-	// Exercise 4: Make a program to flush the LEDs from PB0 to PB7 and
-	// repeat, as following figure.
+	
+	// Task 3: Make a program to flush the LEDs from PB0 to PB7 and repeat, as following figure.
 	DDRB = 0xFF;
 		
 	while (1)
